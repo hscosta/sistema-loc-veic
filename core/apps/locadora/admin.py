9ef9c38deb1e456d06f5a3d88a752d0724479b90
Heverton S Costa <hevertonscosta at gmail dot com>
2022-02-09 17:40:37 -0300

    Added models - Cliente, Veiculo, Locacao, Reserva

--- core/apps/locadora/admin.py
+++ core/apps/locadora/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Veiculo, Cliente, Locacao, Reserva
+
+admin.site.register(Veiculo)
+admin.site.register(Cliente)
+admin.site.register(Locacao)
+admin.site.register(Reserva)
+
 
 # Register your models here.
--- core/apps/locadora/models.py
+++ core/apps/locadora/models.py
@@ -1,20 +1,84 @@
+from pyexpat import model
+from unicodedata import decimal
 from django.db import models
 
 
+tipo_pessoa = (
+    ('F', 'Física'),
+    ('J', 'Jurídica'),
+)
 class Veiculo(models.Model):
-    pass
+    proprietario = models.CharField(max_length=255, null=False, blank=False, verbose_name='Proprietário', )
+    renavam = models.CharField(max_length=100, null=False, blank=False, verbose_name='RENAVAM', )
+    placa = models.CharField(max_length=8, null=False, blank=False, )
+    chassi = models.CharField(max_length=35, null=False, blank=False, )
+    marca = models.CharField(max_length=50, )
+    modelo = models.CharField(max_length=50, )
+    ano = models.IntegerField()
+    valor_locacao = models.DecimalField(max_digits=8, decimal_places=2, )
+    data_aquisicao = models.DateField(blank=True, )
+    cor = models.CharField(max_length=50, )
+    numero_portas = models.IntegerField()
+    motor = models.IntegerField()
+    cambio = models.CharField(max_length=50, )
+    combustivel = models.CharField(max_length=50, )
+    nivel_tanque = models.DecimalField(max_digits=2, decimal_places=2, )
+    km_atual = models.DecimalField(max_digits=8, decimal_places=2, )
+    disponibilidade = models.IntegerField()
+    observacao = models.TextField()
+
 
 class Cliente(models.Model):
-    pass
+    nome = models.CharField(max_length=255, null=False, blank=False, )
+    razao_social = models.CharField(max_length=255, null=False, blank=False, )
+    cpf = models.CharField(max_length=11, null=False, blank=False, )
+    rg = models.CharField(max_length=20, )
+    dt_nascimento = models.DateField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20, )
+    celular = models.CharField(max_length=20, )
+    endereco = models.CharField(max_length=100, )
+    cnh_numero = models.CharField(max_length=30, )
+    cnh_numregistro = models.CharField(max_length=30, )
+    cnh_validade = models.DateField()
+    cnh_categoria = models.CharField(max_length=5, )
+    observacao = models.TextField()
+    situacao = models.IntegerField()
+    cnpj = models.CharField(max_length=15, )
+    insc_estadual = models.CharField(max_length=20, )
+    insc_municipal = models.CharField(max_length=20, )
+    tipo_pessoa = models.CharField(max_length=1, choices=tipo_pessoa, )
+    senha = models.CharField(max_length=50, )
+
+    def __str__(self):
+        return f'{self.nome}'
 
 
 class FormPag(models.Model):
-    pass
+    forma = models.CharField(max_length=50, null=False, blank=False, )
+    observacao = models.TextField()
 
+    def __str__(self):
+        return f'{self.forma}'
 
 class Locacao(models.Model):
-    pass
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, )
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, )
+    datahora_locacao = models.DateTimeField(auto_now_add=True, )
+    datahora_prevista_devolucao = models.DateTimeField()
+    datahora_devolucao = models.DateTimeField()
+    valor_total = models.DecimalField(max_digits=8, decimal_places=2, )
+    qtde_dias = models.IntegerField()
+    km_devolucao = models.DecimalField(max_digits=8, decimal_places=2, )
+
+    def __str__(self):
+        return f'{self.cliente.nome} - {self.veiculo.placa} - {self.veiculo.modelo}'
 
 
-class Fila(models.Model):
-    pass
+class Reserva(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, )
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, )
+    datahora_reserva = models.DateTimeField(null=False, )
+
+    def __str__(self):
+        return f'{self.datahora_reserva} - {self.veiculo.placa} - {self.veiculo.modelo}'
