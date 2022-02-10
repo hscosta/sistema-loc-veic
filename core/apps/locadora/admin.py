5e8ea729b6c1bf4c3a58546d3cdd44ebb981c718
Heverton S Costa <hevertonscosta at gmail dot com>
2022-02-10 09:55:29 -0300

    Upd .gitignore

--- core/apps/locadora/admin.py
+++ core/apps/locadora/admin.py
@@ -1,10 +1,12 @@
+from inspect import formatargspec
 from django.contrib import admin
-from .models import Veiculo, Cliente, Locacao, Reserva
+from .models import Veiculo, Cliente, Locacao, Reserva, FormPag
 
 admin.site.register(Veiculo)
 admin.site.register(Cliente)
 admin.site.register(Locacao)
 admin.site.register(Reserva)
+admin.site.register(FormPag)
 
 
 # Register your models here.
--- core/apps/locadora/models.py
+++ core/apps/locadora/models.py
@@ -15,23 +15,26 @@ class Veiculo(models.Model):
     marca = models.CharField(max_length=50, )
     modelo = models.CharField(max_length=50, )
     ano = models.IntegerField()
-    valor_locacao = models.DecimalField(max_digits=8, decimal_places=2, )
-    data_aquisicao = models.DateField(blank=True, )
+    valor_locacao = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor da Locação')
+    data_aquisicao = models.DateField(blank=True, verbose_name='Data de Aquisição do Veículo', )
     cor = models.CharField(max_length=50, )
-    numero_portas = models.IntegerField()
-    motor = models.IntegerField()
-    cambio = models.CharField(max_length=50, )
-    combustivel = models.CharField(max_length=50, )
-    nivel_tanque = models.DecimalField(max_digits=2, decimal_places=2, )
+    numero_portas = models.IntegerField(default=2, verbose_name='Número de Portas', )
+    motor = models.IntegerField(default=1, )
+    cambio = models.CharField(max_length=50, verbose_name='Câmbio')
+    combustivel = models.CharField(max_length=50, verbose_name='Tipo de combustível', )
+    nivel_tanque = models.DecimalField(max_digits=2, decimal_places=2, default=0.00, verbose_name='Nível do Tanque', )
     km_atual = models.DecimalField(max_digits=8, decimal_places=2, )
-    disponibilidade = models.IntegerField()
-    observacao = models.TextField()
+    disponibilidade = models.IntegerField(default=1, )
+    observacao = models.TextField(blank=True, verbose_name='Observação',)
+
+    def __str__(self):
+        return f'{self.marca} - {self.modelo} - {self.placa}'
 
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False, )
-    razao_social = models.CharField(max_length=255, null=False, blank=False, )
-    cpf = models.CharField(max_length=11, null=False, blank=False, )
+    razao_social = models.CharField(max_length=255, null=True, blank=False, )
+    cpf = models.CharField(max_length=11, null=True, blank=False, )
     rg = models.CharField(max_length=20, )
     dt_nascimento = models.DateField()
     email = models.EmailField()
@@ -42,34 +45,42 @@ class Cliente(models.Model):
     cnh_numregistro = models.CharField(max_length=30, )
     cnh_validade = models.DateField()
     cnh_categoria = models.CharField(max_length=5, )
-    observacao = models.TextField()
+    observacao = models.TextField(null=True, )
     situacao = models.IntegerField()
-    cnpj = models.CharField(max_length=15, )
-    insc_estadual = models.CharField(max_length=20, )
-    insc_municipal = models.CharField(max_length=20, )
+    cnpj = models.CharField(max_length=15, null=True, )
+    insc_estadual = models.CharField(max_length=20, null=True, )
+    insc_municipal = models.CharField(max_length=20, null=True, )
     tipo_pessoa = models.CharField(max_length=1, choices=tipo_pessoa, )
-    senha = models.CharField(max_length=50, )
+    senha = models.CharField(max_length=50, null=True, )
 
     def __str__(self):
         return f'{self.nome}'
 
 
 class FormPag(models.Model):
-    forma = models.CharField(max_length=50, null=False, blank=False, )
-    observacao = models.TextField()
+    forma = models.CharField(max_length=50, null=False, blank=False, verbose_name='Descrição da Forma de Pag.:', )
+    observacao = models.TextField(blank=True, verbose_name='Observação', )
+
+    class Meta:
+        verbose_name = 'Forma de Pagamento'
+        verbose_name_plural = 'Formas de Pagamento'
 
     def __str__(self):
-        return f'{self.forma}'
+        return f'{self.name}'
 
 class Locacao(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, )
-    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, )
-    datahora_locacao = models.DateTimeField(auto_now_add=True, )
-    datahora_prevista_devolucao = models.DateTimeField()
-    datahora_devolucao = models.DateTimeField()
-    valor_total = models.DecimalField(max_digits=8, decimal_places=2, )
-    qtde_dias = models.IntegerField()
-    km_devolucao = models.DecimalField(max_digits=8, decimal_places=2, )
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Nome do Cliente', )
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE, verbose_name='Veículo', )
+    datahora_locacao = models.DateTimeField(auto_now_add=True, verbose_name='Data / Hora da Locação', )
+    datahora_prevista_devolucao = models.DateTimeField(verbose_name='Data / Hora prevista p/ Devolução', )
+    datahora_devolucao = models.DateTimeField(verbose_name='Data / Hora da Devolução', )
+    valor_total = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Valor Total R$', )
+    qtde_dias = models.IntegerField(verbose_name='Qtde. dias locados', )
+    km_devolucao = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Anotação de KM do veículo', )
+
+    class Meta:
+        verbose_name = 'Locação'
+        verbose_name_plural = 'Locações'
 
     def __str__(self):
         return f'{self.cliente.nome} - {self.veiculo.placa} - {self.veiculo.modelo}'
